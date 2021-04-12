cc5aa5e352442582976936b82865dbf1d122cd11
Vakkuri, Elias <elias.vakkuri@cgi.com>
2021-04-12 19:18:45 +0300

    Tehtävät 3.17 asti

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 
 app.use(express.static('build'))
+app.use(express.json())
 
 // Set up morgan library for logging requests and responses
 var morgan = require('morgan')
@@ -20,111 +23,84 @@ app.use(morgan(
     skip: (req, res) => { return req.method !== 'POST' }
   }))
 
-// Set up json handling
-app.use(express.json())
-
 // Set up CORS middleware
 const cors = require('cors')
 app.use(cors())
 
-let persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-1234567",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  }
-]
-
+/*
 app.get('/info', (request, response) => {
   const resString = `
   <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>
-  `
+  <p>${new Date()}</p>`
 
   response.send(resString)
 })
+*/
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  console.log(person)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.post('/api/persons', (request, response) => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(p => p.id))
-    : 0
-
   if (!request.body.name || !request.body.number) {
     return response.status(400).json({
       error: 'content missing'
     })
   }
 
-  const newPerson = {
+  const newPerson = new Person({
     name: request.body.name,
     number: request.body.number,
-    id: generateId(),
-  }
-  newPerson.id = maxId + 1
+  })
 
-  if (persons.map(p => p.name).includes(newPerson.name)) {
-    return response.status(400).json({
-      error: `Person with name ${newPerson.name} is already in the phonebook`
+  newPerson.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
-  }
-
-  persons = persons.concat(newPerson)
-
-  response.json(newPerson)
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
+// Handling unknown endpoints
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+// Handling erroring requests
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformed id' })
+  }
+
+  next(error)
+}
+
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstackopen:${password}@fullstackopen.s8j8p.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+})
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (!newName || !newNumber) {
+  console.log(
+    "Either name or number not provided, fetching all existing persons..."
+  )
+
+  Person.find({}).then(result => {
+    console.log("phonebook:")
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+
+const newPerson = new Person({
+  name: newName,
+  number: newNumber,
+})
+
+
+newPerson.save().then(response => {
+  console.log(`added ${newName} number ${newNumber} to phonebook`)
+  mongoose.connection.close()
+})
+
+
