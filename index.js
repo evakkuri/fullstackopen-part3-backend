c6f28e089d7b2b8b62d28bfbcdce58df1d6449ba
Vakkuri, Elias <elias.vakkuri@cgi.com>
2021-04-13 18:59:35 +0300

    Tehtävät 3.18 asti

--- index.js
+++ index.js
@@ -27,15 +27,15 @@ app.use(morgan(
 const cors = require('cors')
 app.use(cors())
 
-/*
+// API routes
 app.get('/info', (request, response) => {
-  const resString = `
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>`
-
-  response.send(resString)
+  Person.find({}).then(persons => {
+    const resString = `
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>`
+    response.send(resString)
+  })
 })
-*/
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
@@ -54,27 +54,38 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
-  if (!request.body.name || !request.body.number) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
+app.post('/api/persons', (request, response, next) => {
   const newPerson = new Person({
     name: request.body.name,
     number: request.body.number,
   })
 
-  newPerson.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  newPerson.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
@@ -92,6 +103,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'Malformed id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- models/person.js
+++ models/person.js
@@ -17,8 +17,16 @@ mongoose.connect(url, {
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 4
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 4
+  },
 })
 
 personSchema.set('toJSON', {
